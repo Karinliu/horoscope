f97fad7b799c6afe50bf75f2dc8589dd81f98d78
karinliu <karin-liu@hotmail.com>
2022-02-04 18:23:17 +0100

    trying out tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./pages/index.html"],
+  theme: {
+    extend: {
+      // backgroundImage: {
+        // 'hero-pattern': "url('/img/hero-pattern.svg')",
+      // }
+    },
+  },
+  plugins: [],
+}
